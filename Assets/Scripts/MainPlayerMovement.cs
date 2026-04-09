6519b85749dc37331d218efc012a19b76fdd4290
anirudh1107 <anirudhdeep.11@gmail.com>
2026-04-09 13:45:37 +0530

    Added screen bound and flip capability in player controller

--- Assets/Scripts/MainPlayerMovement.cs
+++ Assets/Scripts/MainPlayerMovement.cs
@@ -6,10 +6,14 @@ public class MainPlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 0.05f;
     [SerializeField] private float jumpCooldown = 1.0f;
+    [SerializeField] private GameObject opponentPlayer;
 
     private bool isMoving;
     private Vector2 moveDirection;
     private bool isJumping = false;
+    private bool facingRight = true;
+    private bool canMoveLeft = true;
+    private bool canMoveRight = true;
 
     private Animator animator;
     private Rigidbody rb;
@@ -36,23 +40,56 @@ public class MainPlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        facingRight = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         currentAnimationInfo = animator.GetCurrentAnimatorStateInfo(0);
+        Vector3 playerScreenPosition = Camera.main.WorldToScreenPoint(this.transform.position);
+
+        if(playerScreenPosition.x < 0)
+        {
+            canMoveLeft = false;
+        }
+        else
+        {
+            canMoveLeft = true;
+        }
+
+        if(playerScreenPosition.x > Screen.width)
+        {
+            canMoveRight = false;
+        }
+        else
+        {
+            canMoveRight = true;
+        }
+
+        if(opponentPlayer != null)
+        {
+            Vector3 opponentPosition = opponentPlayer.transform.position;
+            if(this.transform.position.x < opponentPosition.x)
+            {
+                FaceRight();
+            }
+            else
+            {
+                FaceLeft();
+            }
+        }
+
         if (isMoving)
         {
             if (currentAnimationInfo.IsTag("Moving"))
             {
-                if (moveDirection.x > 0)
+                if (moveDirection.x > 0 && canMoveRight)
                 {
                     animator.SetBool("Forward", true);
                     this.transform.Translate(Vector3.right * moveSpeed);
                 }
-                else if (moveDirection.x < 0)
+                else if (moveDirection.x < 0 && canMoveLeft)
                 {
                     animator.SetBool("Backward", true);
                     this.transform.Translate(Vector3.left * moveSpeed);
@@ -86,6 +123,26 @@ public class MainPlayerMovement : MonoBehaviour
             animator.SetBool("Crouch", false);
         }
 
+    }
+
+    private void FaceLeft()
+    {
+        if (facingRight)
+        {
+            facingRight = false;
+            transform.GetChild(0).Rotate(0, 180, 0);
+            animator.SetLayerWeight(1, 1);
+        }
+    }
+
+    private void FaceRight()
+    {
+        if (!facingRight)
+        {
+            facingRight = true;
+            transform.GetChild(0).Rotate(0, 180, 0);
+            animator.SetLayerWeight(1, 0);
+        }
     }
 
     IEnumerator JumpCooldown()
