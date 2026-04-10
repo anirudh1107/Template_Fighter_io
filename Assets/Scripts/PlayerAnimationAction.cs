573885f95c8512802e735126ee5a15be75b682f0
anirudh1107 <anirudhdeep.11@gmail.com>
2026-04-10 20:45:25 +0530

    Added flip animations

--- Assets/Scripts/PlayerAnimationAction.cs
+++ Assets/Scripts/PlayerAnimationAction.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 public class PlayerAnimationAction : MonoBehaviour
 {
     [SerializeField] private float jumpForce = 0.05f;
+    [SerializeField] private float flipForce = 0.1f;
     private GameObject playerCapsule;
     private Rigidbody rb;
 
     private void Awake() {
         playerCapsule = this.transform.parent.gameObject;
+        Debug.Log("Player capsule found: " + playerCapsule.name);
         rb = playerCapsule.GetComponent<Rigidbody>();
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -31,5 +33,15 @@ public class PlayerAnimationAction : MonoBehaviour
             return;
         }
         rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    public void FlipAnimationAction()
+    {
+        if (rb == null)
+        {
+            Debug.LogError("Rigidbody component not found on player capsule.");
+            return;
+        }
+        rb.AddForce(Vector3.up * flipForce, ForceMode.Impulse);
     }
 }
